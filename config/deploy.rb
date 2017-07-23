ba6f3817cbd1b5bf2a0bf4dd3c52bb044dfb4591
Muhammad Firhard Roslan <firhard@gmail.com>
2017-07-23 14:42:45 +0800

    Frefall

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.4.0' # Edit this if you are using MRI Ruby
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
